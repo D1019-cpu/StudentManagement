92f67a71f4060e2132c6d9f8e4446b53318b584f
longnguyen175lit@gmail.com <longnguyen175lit@gmail.com>
2023-08-16 22:37:34 +0700

    Update Interface

--- student_management/student.py
+++ student_management/student.py
@@ -5,7 +5,7 @@ def add_student():
     """Add Student Function"""
     print('-'*80)
     print("|", "Add Student".center(76, ' '), "|",sep=' ')
-    print('-'*80)
+    
     # struct of the student
     infor = {
         'id': '',
@@ -55,37 +55,54 @@ def find_student(id):
 
 def show_students():
     """Show List Students Function"""
-    print('-'*40)
+    print('-'*80)
+    print("|", "List Students".center(76, ' '), "|", sep=' ')
     for i in d.list_students:
-        print(f"{i['id']:4} {i['name']:25} {i['age']:4} {i['gender']:7} {i['address']:15} {i['CPA']:8}")
-
+        print(f"| {i['id']:4} | {i['name']:25} | {i['age']:4} | {i['gender']:7} | {i['address']:15} {i['CPA']:8} |")
+    print('-'*80)
 
 def edit_student():
     """Edit Student Function"""
-    print("-"*40)
-    id = input("Enter ID of the student: ")
+    print("-"*80)
+    print("|", "Edit Student".center(76, ' '), "|", sep=' ')
+
+    print("|Enter ID of the student: ".ljust(78, ' '), "|", sep=' ') 
+    id = input("|ID: ")
     student = find_student(id)
     # print(student)
     if student != False:
         # return [index,item]
         # Edit Name, age, gender, address, CPA
-        student[1]['name'] = input("Enter name of the student: ") 
-        student[1]['age'] = input("Enter age of the student: ")
-        student[1]['gender'] = input("Enter gender of the student: ")
-        student[1]['address'] = input("Enter address of the student: ")
-        student[1]['CPA'] = input("Enter CPA of the student: ") 
+        print("|Enter name of the student: ".ljust(78, ' '), "|", sep=' ')
+        student[1]['name'] = input("|Name: ") 
+
+        print("|Enter age of the student: ".ljust(78, ' '), "|", sep=' ')
+        student[1]['age'] = input("|Age: ")
+        
+        print("|Enter gender of the student: ".ljust(78, ' '), "|", sep=' ')
+        student[1]['gender'] = input("|Gender: ")
+
+        print("|Enter address of the student: ".ljust(78, ' '), "|", sep=' ')
+        student[1]['address'] = input("|Address: ")
+        
+        print("|Enter CPA of the student: ".ljust(78, ' '), "|", sep=' ')
+        student[1]['CPA'] = input("|CPA: ") 
+        
         d.list_students[student[0]] = student[1]
     else:
-        print("ID of the student does not exist!!!")
-
+        print("|", "ID of the student does not exist!!!".center(76, ' '), "|", sep=' ')
+    print("-"*80)
 
 def delete_student():
     """Delete Student Function"""
-    print('-'*40)
-    id = input("Enter ID of the student: ")
+    print("-"*80)
+    print("|", "Delete Student".center(76, ' '), "|", sep=' ')
+    print("|Enter ID of the student: ".ljust(78, ' '), "|", sep=' ')
+    id = input("|ID: ")
     student = find_student(id)
     if student != False:
         d.list_students.remove(student[1])
-        print("Delete Student Successfully!")
+        print("|", "Delete Student Successfully!".center(76, ' '), "|", sep=' ')
     else:
-        print("Deleted candidate could not be located.")
+        print("|", "Deleted candidate could not be located.".center(76, ' '), "|", sep=' ')
+    print("-"*80)
